6d8c485c1a49cec1e4aa3743ccf7687066e55761
Stonehagen <steinhagen@mailbox.org>
2023-03-05 22:26:27 +0100

    add messages to index

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages });
 });
 
 module.exports = router;
